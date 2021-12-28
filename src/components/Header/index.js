a665ef6c284e1556cf0ea9fd5581f1484c865ba4
Julian Nunes <julian.jgn@gmail.com>
2021-12-27 23:59:37 -0300

    Favicon, CSS animation and real clock time

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,77 @@
+import styled, { keyframes } from 'styled-components';
+
 import './style.css'
 
 import logo from '../../imgs/logo.svg';
 
 function Header() {
-    return(
+  return(
       <header>
         <div className="clock-base">
-          <div className="second-line"></div>
-          <div className="minute-line"></div>
-          <div className="hour-line"></div>
+          <SecondLine></SecondLine>
+          <MinuteLine></MinuteLine>
+          <HourLine></HourLine>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     )
 }
+
+const d = new Date();
+const nowSeconds = d.getSeconds() * 6 - 90;
+const nowMinutes = d.getMinutes() * 6 - 90;
+const nowHour = d.getHours()*30 - 90;
+
+const rotateSeg = keyframes`
+  from {
+    transform: rotate(${nowSeconds }deg);
+  }
+  to {
+    transform: rotate(${nowSeconds + 360}deg);
+  }
+`;
+
+const rotateMin = keyframes`
+  from {
+    transform: rotate(${nowMinutes}deg);
+  }
+  to {
+    transform: rotate(${nowMinutes + 360}deg);
+  }
+`;
+
+const rotateHour = keyframes`
+  from {
+    transform: rotate(${nowHour}deg);
+  }
+  to {
+    transform: rotate(${nowHour + 360}deg);
+  }
+`;
+
+
+const SecondLine = styled.div`
+  background-color: #9b6dbf;
+  height: 1px;
+  margin-left: -1px;
+  width: 36%;
+  animation: ${rotateSeg} 60s linear infinite;
+`;
+
+const MinuteLine = styled.div`
+  background-color: #9b6dbf;
+  height: 4px;
+  margin-left: -1px;
+  width: 34%;
+  animation: ${rotateMin} 3600s linear infinite;
+`;
+
+const HourLine = styled.div`
+  background-color: #633585;
+  height: 6px;
+  margin-left: -3px;
+  width: 20%;
+  animation: ${rotateHour} 43200s linear infinite;
+`;
 
 export default Header;  
