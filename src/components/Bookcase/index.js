ca439a07556ebd0148843f6ace6d5d1c2a59a486
Julian Nunes <julian.jgn@gmail.com>
2021-12-27 22:12:46 -0300

    Adjust no-unused-vars

--- src/components/Bookcase/index.js
+++ src/components/Bookcase/index.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { ReactSortable } from "react-sortablejs";
 
